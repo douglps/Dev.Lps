4e87cf4a909e62bd193802b1fbcd1350c579a49d
douglps <doug.lp@live.com>
2025-05-25 14:14:42 -0300

    sitemap config

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://devlps.vercel.app', // 🔁 substitua pela URL real do seu site
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+};
